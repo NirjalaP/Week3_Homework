9aaf4b0c9d3c038380a00c3877d30af2cd57d767
NirjalaP <nirjala.prajapati11@gmail.com>
2024-12-12 07:45:14 -0800

    Added user interaction with table cells

--- js/activity.js
+++ js/activity.js
@@ -16,3 +16,40 @@ $(document).ready(function () {
     });
   });
   
+  //User interaction with table cells
+$(document).ready(function () {
+    $("td").each(function () {
+      if ($(this).text() !== "Not Available") {
+        $(this).addClass("selectable");
+      }
+    });
+    $("td").click(function () {
+      // user select a table data cell
+      var content = $(this).text(); // get content of cell
+  
+  
+      if (content != "Not Available") {
+        // check if content does not contain a particular string
+        $(this).toggleClass("tdhighlight"); // add or remove class when cell is selected
+  
+  
+        if ($(this).hasClass("tdhighlight")) {
+          // check if selected cell has class
+          $("#displaySelected").css("visibility", "visible"); // make display box visible
+          $("#displaySelected").css("margin-top", "2em"); // add spaces above display box
+          $("#result").append("<p>" + content + "</p>"); // add child element with contents of cell
+        } else {
+          // if selected cell doesn't have class
+          $("#result p:contains(" + content + ")").remove(); // remove child element
+  
+  
+          if ($("#result p").length == false) {
+            // check if there are any child elements within parent
+            $("#displaySelected").css("visibility", "hidden"); // make display box hidden
+            $("#displaySelected").css("margin-top", "0"); // remove spaces above display box
+          }
+        }
+      }
+    });
+  });
+    

68a82cfe0c621637d9eaeabab006b4fc8b165adb
NirjalaP <nirjala.prajapati11@gmail.com>
2024-12-12 07:49:26 -0800

    Week 5_Day3_Homework

--- js/activity.js
+++ js/activity.js
@@ -1,22 +1,4 @@
-$(document).ready(function () {
-    $("td").each(function () {
-      if ($(this).text() !== "Not Available") {
-        $(this).addClass("selectable");
-      }
-    });
-    $("td").click(function () {
-      // user select a table data cell
-      var content = $(this).text(); // get content of cell
-  
-  
-      if (content != "Not Available") {
-        // check if content does not contain a particular string
-        $(this).toggleClass("tdhighlight"); // add or remove class when cell is selected
-      }
-    });
-  });
-  
-  //User interaction with table cells
+//User interaction with table cells
 $(document).ready(function () {
     $("td").each(function () {
       if ($(this).text() !== "Not Available") {
@@ -53,4 +35,3 @@ $(document).ready(function () {
     });
   });
     
-  

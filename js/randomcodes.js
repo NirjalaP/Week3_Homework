b5c8e521d24e43c8d3c6abb5dc30d727d346c3d5
NirjalaP <nirjala.prajapati11@gmail.com>
2024-12-11 22:42:32 -0800

    Week 4_Day1_Homework

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -3,7 +3,7 @@ function generateCode() {
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let code = '';
     for (let i = 0; i < 8; i++) {
-      code += chars.charAt(Math.floor(Math.random() * chars.length));
+      code += str.charAt(Math.floor(Math.random() * str.length));
     }
     return code;
   }
